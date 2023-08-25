33925914639f33fb6b587cccc42b0b0d6ee0ecbe
sneha1012 <snehamaurya1012@gmail.com>
2023-08-25 23:39:23 +0530

    dONE

---
+++ download.sh
@@ -0,0 +1,8 @@
+wget https://ftp.ebi.ac.uk/empiar/world_availability/10045/data/ribosomes/Tomograms/05/IS002_291013_005.coords
+wget https://ftp.ebi.ac.uk/empiar/world_availability/10045/data/ribosomes/Tomograms/05/IS002_291013_005.mrc
+wget https://ftp.ebi.ac.uk/empiar/world_availability/10045/data/ribosomes/Tomograms/06/IS002_291013_006.coords
+wget https://ftp.ebi.ac.uk/empiar/world_availability/10045/data/ribosomes/Tomograms/06/IS002_291013_006.mrc
+wget https://ftp.ebi.ac.uk/empiar/world_availability/10045/data/ribosomes/Tomograms/07/IS002_291013_007.coords
+wget https://ftp.ebi.ac.uk/empiar/world_availability/10045/data/ribosomes/Tomograms/07/IS002_291013_007.mrc
+wget https://ftp.ebi.ac.uk/empiar/world_availability/10045/data/ribosomes/Tomograms/08/IS002_291013_008.coords
+wget https://ftp.ebi.ac.uk/empiar/world_availability/10045/data/ribosomes/Tomograms/08/IS002_291013_008.mrc
